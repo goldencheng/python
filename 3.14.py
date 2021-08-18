508df8fb7b11fe45d52a1ef3d92d280742067cb2
goldencheng <88918069+goldencheng@users.noreply.github.com>
2021-08-18 13:54:00 +0800

    Add files via upload

---
+++ 3.14.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 14 18:25:07 2021
+
+@author: twson
+"""
+o = 3.14
+q = 2
+
+e=float(input('how long is the radius?'))
+print(    )
+print('cicle area is ')
+print(e**q*o)
+print(    )
+print('perimeter is')
+print(e*q*o)
