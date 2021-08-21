2b76d6f4aea323bc5340825322a80e7c2e979c87
goldencheng <88918069+goldencheng@users.noreply.github.com>
2021-08-21 10:43:38 +0800

    Add files via upload

--- guess_my_number.py
+++ guess_my_number.py
@@ -8,11 +8,221 @@ to input a number until he/she gets it
 """
 
 # This number controls when to stop the game
-SECRET = 32
-
-
+SECRET1 = 82
+EEEE = 99
+print('guess a numbur 0-99')
 def main():
-    pass
+    ans = int(input('guess'))
+    while SECRET1 != ans:
+        if ans > EEEE:
+            print('out of 99')
+        if ans > SECRET1:
+            print('too big!')
+        if ans < SECRET1:
+            print('too small!')
+        ans = int(input('guess'))
+    print('you got it!:',end='')
+    print( SECRET1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ### DO NOT EDIT THE CODE BELOW THIS LINE ###
