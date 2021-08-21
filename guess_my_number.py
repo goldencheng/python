7e7270aae73af99c10c7d3ec684e8673259372e0
goldencheng <88918069+goldencheng@users.noreply.github.com>
2021-08-21 10:40:32 +0800

    Add files via upload

---
+++ guess_my_number.py
@@ -0,0 +1,21 @@
+"""
+File: guess_my_number.py
+Name:
+-----------------------------
+This program plays a Console game
+"Guess My Number" which asks user
+to input a number until he/she gets it
+"""
+
+# This number controls when to stop the game
+SECRET = 32
+
+
+def main():
+    pass
+
+
+### DO NOT EDIT THE CODE BELOW THIS LINE ###
+
+if __name__ == '__main__':
+    main()
