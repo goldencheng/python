6cfe445f71a273439b405005698f3a8435b79ad9
goldencheng <88918069+goldencheng@users.noreply.github.com>
2021-08-22 10:13:36 +0800

    Add files via upload

--- if else.py
+++ if else.py
@@ -5,7 +5,21 @@ Created on Sat Aug 21 10:26:22 2021
 @author: twson
 """
 e=float(input('point?'))
-if e>=60:
-    print("good")
-else:
-    print("no")
+
+while e>100 or e<0:
+   print("no way")
+   e = int(input('point?'))
+if 101>e>=90:
+    print("a")
+if 80<=e<90:
+    print("b")
+if 70<=e<80:
+    print("c")
+if 60<=e<70:
+    print("d")
+if e<=60:
+    print("e")
+
+
+
+    
