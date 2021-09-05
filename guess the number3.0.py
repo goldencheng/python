29f7294951108abe397fc1e8e514d5e66f47f5d1
goldencheng <88918069+goldencheng@users.noreply.github.com>
2021-09-05 17:40:12 +0800

    Add files via upload

--- guess the number3.0.py
+++ guess the number3.0.py
@@ -5,14 +5,13 @@ Created on Sat Sep  4 16:18:29 2021
 @author: twson
 
 """
-ppo=int(input('range1?'))
-q=int(input('range2?'))
-pp=int(input('chance?'))
 
-
+ppo=int(input('range1(lower)~'))
+q=int(input('range2(uper)~'))
+pp=int(input('chance~'))
 import random
 SECRET1=random.randint(ppo,q)
-(type(float(SECRET1)))
+print('        ')
 EEEE = q
 print('guess a numbur',end='')
 print(ppo,end='')
@@ -22,45 +21,43 @@ print('.')
 print('you have ',end='')
 print(pp,end='')
 print(" times to guess!!")
-
-
-ans = float(input('enter'))
+ans = float(input('#enter the number#'))
 for i in range(pp-1):
-        if SECRET1!=ans :
-            
-            if ans > EEEE:
-                print('out of the range and ')
-            if ans > SECRET1:
-                print('too big!')
-                ans = int(input('guess'))
-            if ans < SECRET1:
-                print('too small!')
-                ans = int(input('guess'))
+       if SECRET1!=ans :
+                
+         if ans > EEEE:
+                    print('out of the range and ')
+         if ans > SECRET1:
+                    print('too big!')
+                    ans = int(input('guess'))
+         if ans < SECRET1:
+                    print('too small!')
+                    ans = int(input('#enter the number#'))
 if SECRET1==ans:
-    print('you win!')
-    print('anser is ',end='')
-    print(SECRET1)
+   print('you win!')
+   print('anser is ',end='')
+   print(SECRET1)
 else:
-    print('you lose!')
-    print('anser is',end='')
-    print(SECRET1)
-    
-    
-
-
-      
+   print('you lose!')
+   print('anser is ',end='')
+   print(SECRET1)
         
-         
-
+        
     
 
-
-
-
-         
+          
+            
+             
     
         
     
+    
+    
+    
+             
         
-       
-
+            
+        
+            
+           
+    
